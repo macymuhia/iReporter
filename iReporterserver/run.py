527e71ec2c73721b0bd0939e840a541fa790ae8d
macymuhia <macymuhia@gmail.com>
2018-11-29 13:21:46 +0300

    [finishes #162210015] API to create a redflag record

--- iReporterserver/run.py
+++ iReporterserver/run.py
@@ -1,11 +1,13 @@
 import os
 from flask_restful import Api, Resource
 from app import create_app
+from app.api.v1.redflags.views import RedFlagList
 
 config_name = os.getenv("FLASK_ENV", "development")
 app = create_app(config_name)
 api = Api(app)
 
+api.add_resource(RedFlagList, "/redflags")
 
 if __name__ == "__main__":
     app.run()
