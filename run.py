52e92bb1131a2dcf69cace41025af8d26531ebc8
macymuhia <macymuhia@gmail.com>
2018-12-01 15:00:15 +0300

    Move app files to root directory

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
 import os
 from app import create_app
-from app.api.v1 import blueprint
 
 config_name = os.getenv("FLASK_ENV", "development")
 app = create_app(config_name)
-app.register_blueprint(blueprint)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
